54ec3139db34e1e554d631f283ff1d50ac8c6c0a
Afeez Kolade <koladeafeez@ctl.is>
2023-02-22 23:10:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("welcome");
+
+const url = location.search;
+console.log(url);
+    const params = new URLSearchParams(url);
+    let paramObj = {};
+    for (var value of params.keys()) {
+        paramObj[value] = params.get(value);
+    }
+
+console.log(paramObj);
+
+var port = paramObj["port"];
+//location.replace(`https://localhost:${port}${url}`);
