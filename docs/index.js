13988c3725e5eb50e13f9b8f8b83ee45b3db58d8
Kolade  Afeez <koladeafeez60@gmail.com>
2023-03-08 09:49:56 +0100

    Update index.js

--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,14 @@
 console.log("welcome");
 
-const url = location.search;
-console.log(url);
-    const params = new URLSearchParams(url);
-    let paramObj = {};
-    for (var value of params.keys()) {
-        paramObj[value] = params.get(value);
-    }
+//const url = location.search;
+//console.log(url);
+//    const params = new URLSearchParams(url);
+//    let paramObj = {};
+//    for (var value of params.keys()) {
+//        paramObj[value] = params.get(value);
+//    }
 
-console.log(paramObj);
+//console.log(paramObj);
 
-var port = paramObj["port"];
-location.replace(`https://localhost:44379/SCA/Complete${url}`);
+//var port = paramObj["port"];
+//location.replace(`https://localhost:44379/SCA/Complete${url}`);
